1456bdbdd3fbf35292e4b85ed61e097d384b5941
EloyCapdeboscq <eloycade@gmail.com>
2023-08-19 20:37:06 -0300

    termine entregable

---
+++ src/routes/views.router.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import ProductManager from "./managers/productsManager.js";
+import { __dirname } from "../utils.js";
+
+const pmanager = new ProductManager(__dirname + "/files/products.json");
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const listaProductos = await pmanager.getProducts();
+  res.render("home", { listaProductos });
+});
+
+router.get("/realTimeProducts", async (req, res) => {
+  const listaProductos = await pmanager.getProducts();
+  res.render("realTimeProducts", { listaProductos });
+});
+
+export default router;
